51dba0712c7ba6643d989819ac6da642011ea6a2
Akshat1308 <suranaas@rknec.edu>
2023-12-18 23:12:59 +0530

    logout

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,15 +3,15 @@ const {
     registerUser,
     loginUser,
     getUserDetails,
+    logout
 } = require("../controllers/userController.js");
 const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
-
 router.route("/login").post(loginUser);
-
+router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
